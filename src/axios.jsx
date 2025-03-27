2501bccce82b198410cbf3830c63de0124783207
Pranaysalavadhi <pranaysalavadhi@gmail.com>
2025-03-27 11:36:09 +0530

    Updated Axios baseURL to EC2 instance

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://ec2-3-85-81-74.compute-1.amazonaws.com:8080/api",
 });
 delete API.defaults.headers.common["Authorization"];
 export default API;
